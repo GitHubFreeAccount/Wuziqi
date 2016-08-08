d929ee15d2fde7db89413e4138121ee5b0465c69
GitHubFreeAccount <qianshuiqianshui@gmail.com>
2016-08-08 17:07:18 +0800

    添加打印

--- src/com/wzx/backgammon/utils/AppUtil.java
+++ src/com/wzx/backgammon/utils/AppUtil.java
@@ -1,12 +1,16 @@
 package com.wzx.backgammon.utils;
 
 import android.content.Context;
+import android.util.Log;
 
 public class AppUtil {
+	public static final String TAG="AppUtil";
 	
-	 public static int dip2px(Context context,float dpValue) {
+	 public static int dip2px(Context context,float dpValue) {		
 	        final float scale =context.getResources().getDisplayMetrics().density;
-	        return (int) (dpValue * scale);
+	        int px=(int)(dpValue * scale);
+	        Log.e(TAG, "dip2px dp="+dpValue+"  px="+px);	        
+	        return px;
 	}
 
 }
